0668b52353bd724fdc5e17edce97cb682682988e
Romário Cordeiro Cabó <romariocabo2012@gmail.com>
2025-06-04 14:10:56 -0300

    first commit

---
+++ src/test/java/com/br/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
